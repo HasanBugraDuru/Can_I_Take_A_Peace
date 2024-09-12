c4e5e2befcfa23ed7f8760542852183982070c89
Hasan Buğra Duru <hasanbugraduru@hotmail.com>
2024-09-13 02:49:23 +0300

    main menu

---
+++ Assets/Scripts/SO/Datas.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Datas", menuName = " Datas")]
+
+public class Datas : ScriptableObject
+{
+    public float MusicValue;
+    public float SoundValue;
+    public bool EscOpened,isPaused, MusicOn,SoundOn;
+
+}
